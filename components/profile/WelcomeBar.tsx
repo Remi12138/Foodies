1aa2111cba079dc24eff34eb62e3b282b31c4c8e
jamie <liujamie23@gmail.com>
2024-11-11 21:52:39 -0500

    refactor: enhance WelcomeBar component with user avatar and additional info display

--- components/profile/WelcomeBar.tsx
+++ components/profile/WelcomeBar.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { HelloWave } from "@/components/common/HelloWave";
@@ -8,18 +8,48 @@ export default function WelcomeBar() {
   const { user } = useUserStore();
 
   return (
-    <ThemedView style={styles.titleContainer}>
-      <ThemedText type="title">Hola</ThemedText>
-      <ThemedText type="title">{user && user.name}</ThemedText>
+    <ThemedView style={styles.container}>
+      <Image
+        source={
+          user?.avatar !== ""
+            ? { uri: user?.avatar }
+            : require("@/assets/images/avatar-placeholder.jpg")
+        }
+        style={styles.avatar}
+      />
+      <View style={styles.userInfoContainer}>
+        <ThemedText style={styles.userNameText}>{user?.name}</ThemedText>
+        <ThemedText style={styles.userCidText}>@ {user?.cid}</ThemedText>
+      </View>
       <HelloWave />
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
     flexDirection: "row",
     alignItems: "center",
-    gap: 8,
+    gap: 10,
+  },
+  avatar: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    overflow: "hidden",
+  },
+  userInfoContainer: {
+    paddingVertical: 8,
+    flexDirection: "column",
+  },
+  userNameText: {
+    fontFamily: "SpaceMonoB",
+    fontSize: 24,
+    paddingTop: 8,
+  },
+  userCidText: {
+    fontFamily: "SpaceMonoI",
+    fontSize: 16,
+    lineHeight: 20,
   },
 });
