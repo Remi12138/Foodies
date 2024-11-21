6d51733a6f8176400da4966b68dcf0ee82afa8f0
Jamie Liu <liujamie23@gamil.com>
2024-11-21 16:00:39 -0500

    Update placeholder for RTT Yelp ID input in post creation

--- components/community/post/postCreation/PostCreate.tsx
+++ components/community/post/postCreation/PostCreate.tsx
@@ -65,7 +65,7 @@ function PostCreate() {
           />
           <TextInput
             style={styles.rttInput}
-            placeholder={draft.rtt_yelp_id}
+            placeholder="EvJqRISUz3IqSfIW0lygDg"
             value={draft.rtt_yelp_id}
             onChangeText={handleRttYelpIdChange}
           />
