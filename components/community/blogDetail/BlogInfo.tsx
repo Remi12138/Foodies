87d6df514a3870f0ef4a25a11383f96aeb85c677
jamie <liujamie23@gmail.com>
2024-12-01 22:09:32 -0500

    Add like count functionality and update blog likes on toggle

--- components/community/blogDetail/BlogInfo.tsx
+++ components/community/blogDetail/BlogInfo.tsx
@@ -4,7 +4,11 @@ import { Ionicons } from "@expo/vector-icons";
 import { ThemedView } from "@/components/ThemedView";
 import BlogAuthor from "@/components/community/blogDetail/BlogAuthor";
 import { useCollectionStore } from "@/zustand/collections";
-import { updateFavoriteBlogIdFromServer } from "@/utils/blogs/favorites";
+import {
+  updateBlogCoverLikesFromServer,
+  updateBlogLikesFromServer,
+  updateFavoriteBlogIdFromServer,
+} from "@/utils/blogs/favorites";
 import { Blog, BlogCover } from "@/zustand/blog";
 import { FIREBASE_AUTH } from "@/firebaseConfig";
 import { ThemedText } from "@/components/ThemedText";
@@ -19,6 +23,7 @@ function BlogInfo({
   const { blogIds, setBlogIds, addBlogCover, removeBlogCover } =
     useCollectionStore();
   const [isLiked, setIsLiked] = useState<boolean>(isInitiallyLiked);
+  const [likecount, setLikecount] = useState<number>(blog.likes_count);
   const currentUser = FIREBASE_AUTH.currentUser;
 
   const toggleLike = async () => {
@@ -30,13 +35,18 @@ function BlogInfo({
           setBlogIds(newBlogIds);
           // sync with server
           updateFavoriteBlogIdFromServer(currentUser.uid, blog.id, "remove");
+          updateBlogCoverLikesFromServer(blog.id, "decrease");
+          updateBlogLikesFromServer(blog.author_uid, blog.id, "decrease");
           // remove blog cover from local
           removeBlogCover(blog.id);
+          setLikecount(likecount - 1);
         } else {
           const newBlogIds = [blog.id, ...blogIds];
           setBlogIds(newBlogIds);
           // sync with server
           updateFavoriteBlogIdFromServer(currentUser.uid, blog.id, "add");
+          updateBlogCoverLikesFromServer(blog.id, "increase");
+          updateBlogLikesFromServer(blog.author_uid, blog.id, "increase");
           // add blog cover from local
           const blogCover = {
             blog_id: blog.id,
@@ -47,6 +57,7 @@ function BlogInfo({
             author: blog.author,
           } as BlogCover;
           addBlogCover(blogCover);
+          setLikecount(likecount + 1);
         }
       } catch (error) {
         setIsLiked(!isLiked);
@@ -67,7 +78,7 @@ function BlogInfo({
           />
         </TouchableOpacity>
         <ThemedView style={styles.likecount}>
-          <ThemedText>{blog.likes_count}</ThemedText>
+          <ThemedText>{likecount}</ThemedText>
         </ThemedView>
         <TouchableOpacity style={styles.shareButton}>
           <Ionicons name="share-social-outline" size={24} color="#000" />
