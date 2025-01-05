a5336a5b6fc5395a42180165b18bf287e61114df
Xianjing Huang <145180192+Remi12138@users.noreply.github.com>
2025-01-05 12:02:06 -0500

    Update Config

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -11,13 +11,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_ept_U2lfvhw4bWy7KaQ06-egt8YZL38",
-  authDomain: "tapteam-foodies.firebaseapp.com",
+  apiKey: "xxx",
+  authDomain: "xxx.com",
   projectId: "tapteam-foodies",
   storageBucket: "tapteam-foodies.firebasestorage.app",
-  messagingSenderId: "263000789674",
-  appId: "1:263000789674:web:f0997aefadb59d8c42b6d3",
-  measurementId: "G-YHEZ2XY5BT",
+  messagingSenderId: "xxx",
+  appId: "xxx",
+  measurementId: "xxx",
 };
 
 // Initialize Firebase
